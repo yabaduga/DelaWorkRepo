90e6f6c93dd65039fa58447096c40c0c22c1dfe1
manuel@antunes.me <manuel@antunes.me>
2019-03-23 23:16:46 +0100

    travail sur le livre "programmer en java - Delannoy"

---
+++ src/Exemple.java
@@ -0,0 +1,16 @@
+public class Exemple {
+    public static void main (String [] args)
+    {
+        int n;
+        double x;
+        n=5;
+        x = 2*n +1.5;
+        System.out.println("n = "+ n);
+        System.out.println("x = "+ x);
+        double y;
+        y = n * x+12;
+        System.out.println("valeur de y "+ y);
+    }
+}
+// a+b*x va afficher a puis le résultat de b*x
+// pour avoir le résultat de l'opération/expression il faut (a+b*x)
---
+++ src/PremProg.java
@@ -0,0 +1,14 @@
+public class PremProg {
+    public static void main(String[] args) {
+        System.out.println("Mon premier programme Java");
+
+    }
+}
+/**
+ * static : précise que la méthode main de la classe PremProg n'est pas liée à une instance (objet) particulière de la classe
+        * main : c'est la fonction principale
+        * String [] args : permet de récuperer des arguments transmis au programme au moment du lancement
+        * public 'dans la class PremProg : sert à définir les droits d'accès des autres classes (en fait de leurs méthodes) à la classe PremProg
+        * public dans public static main est obligatoire pour ue le programme puisse s'exécuter - la machine virtuelle accède ainsi à la méthode main
+        * system désigne une classe dans lequel est définit le champ donnée out, représentant la fenetre console
+        */
